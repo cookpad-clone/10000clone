0433e8beceac973a69b055d3349b393d146f5c55
nobinson20 <nobinson20@gmail.com>
2020-05-26 20:16:25 +0900

    최초

---
+++ src/main/java/com/team/clone10000/Clone10000Application.java
@@ -0,0 +1,13 @@
+package com.team.clone10000;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Clone10000Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Clone10000Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/team/clone10000/Clone10000ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.team.clone10000;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Clone10000ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
