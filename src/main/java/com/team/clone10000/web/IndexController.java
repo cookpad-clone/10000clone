b9bc9dec66e538a290443931ac51bf619ce3ed41
Lee Hyemi <devham76@gmail.com>
2020-05-31 16:18:52 +0900

    develop와 충돌 부분 수정

--- src/main/java/com/team/clone10000/web/IndexController.java
+++ src/main/java/com/team/clone10000/web/IndexController.java
@@ -5,8 +5,19 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class IndexController {
+
+    @GetMapping("/")
+    public String index() {
+        return "index";
+    }
+
     @GetMapping("/chef")
     public String chef(){
         return "chef-list";
+    }
+
+    @GetMapping("/story")
+    public String story(){
+        return "story";
     }
 }
